6c765785f950ce5ee4a08426c76fba9035b0b76a
mithrilwoodrat <mithrilwoodrat@gmail.com>
2018-11-18 01:11:16 +0800

    add doc

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # IDS-Auto-Tools
 IDS-Auto-Tools, Tools For Snort Rules gen and pcap edit
+
+## Requeriments
+ * `git clone https://github.com/Mithrilwoodrat/flowsynth`
+ * run `sudo python setup.py install`
--- gen_pcap.py
+++ gen_pcap.py
@@ -3,12 +3,12 @@ import sys
 import re
 import subprocess
 
-template = """flow default tcp example.com:44123 > baidu.com:80 (tcp.initialize;);
+template = """flow default tcp {sip}:{sport} > {dip}:{dport} (tcp.initialize;);
 default > (content:"GET {URL} HTTP/1.1\x0d\x0aHost:baidu.com\x0d\x0aUser-Agent: DogBot\x0d\x0a\x0d\x0a";);
 default < (content:"HTTP/1.1 200 OK\x0d\x0aContent-Length: 26\x0d\x0a\x0d\x0aWelcome to Google.com!\x0d\x0a\x0d\x0a";);"""
 
 def gen_fs(url):
-    fs = template.format(URL=url)
+    fs = template.format(sip='127.0.0.1', sport=44124, dip='220.181.57.216', dport=80, URL=url)
     with open('test.fs' ,'w') as f:
         f.write(fs)
         
